cf4a4bc43bee28bed969785d39348077bb463a66
clgzb9610 <wkim@macalester.edu>
2018-03-08 15:51:20 -0600

    camera turns off when q is types

--- Activity6_OpenCV/camera.py
+++ Activity6_OpenCV/camera.py
@@ -1,10 +1,14 @@
 import cv2
 
 vidCap = cv2.VideoCapture(0)
-for i in range(300):
+while True:
     ret, img = vidCap.read()
     cv2.imshow("Webcam", img)
-    cv2.waitKey(10)   # Waiting may be needed for window updating
+    x = cv2.waitKey(10)
+    char = chr(x & 0xFF)
+    if char == 'q':
+        break
+
 
 cv2.destroyAllWindows()
 vidCap.release()
